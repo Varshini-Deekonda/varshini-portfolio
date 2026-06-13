38e40c2389d08817a9b3a518546b002115ed5b86
Varshini-Deekonda <deekondavarshini10@gmail.com>
2026-06-13 20:58:18 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Varshini Deekonda - Portfolio',
+  description: 'Data Engineer Portfolio',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
