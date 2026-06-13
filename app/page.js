e5118990a74f68f1ccb49daa18d5d2863c59f706
Varshini-Deekonda <deekondavarshini10@gmail.com>
2026-06-13 21:08:22 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,14 @@
 "use client";
-import {Portfolio} from "./portfolio";
+
+import dynamic from "next/dynamic";
+
+// This tells Next.js to only load your portfolio code directly inside the browser,
+// preventing the build server from crashing!
+const PortfolioComponent = dynamic(
+  () => import("./portfolio").then((mod) => mod.Portfolio),
+  { ssr: false }
+);
 
 export default function Home() {
-  return <Portfolio />;
+  return <PortfolioComponent />;
 }
